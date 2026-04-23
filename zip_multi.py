a88f6f5ea9561aac362d12b64d5a4aa15265c2aa
misu.c <misu.cmake@outlook.com>
2026-04-22 17:34:21 -0700

    print prototype

--- zip_multi.py
+++ zip_multi.py
@@ -1,12 +1,45 @@
 from pathlib import Path
 import subprocess
+import os
 
-print("enter root directory path: ")
-root_path = "H:\Not Backed Up\\test zip\Hello"
+def main():
+    print("enter root directory path: ")
+    root_path = Path(input())
 
-print("enter zip file destination: ")
-zip_destination = "H:\Not Backed Up\\test zip\zipped hello"
+    print("enter zip file destination: ")
+    zip_root = Path(input())
 
-for sub_directory in root_path.iterdir():
-    if subdir.is_dir():
-        print(sub_directory)
+    for sub_directory in root_path.iterdir():
+        sub_directory_name = sub_directory.name
+        
+        zip_location = zip_root.joinpath(sub_directory_name)
+        if sub_directory.is_dir():
+            dir_size = len(os.listdir(sub_directory))
+            if(dir_size > 0):
+                zip_tool = Zipper(zip_location)     
+                for deep_directory in sub_directory.iterdir():
+                    # print(deep_directory)
+                    zip_tool.append_argument(deep_directory)
+                # print(zip_tool.get_argument())
+        else:
+            zip_tool = Zipper(zip_location) 
+            zip_tool.append_argument(sub_directory)
+            # print(zip_tool.get_argument())
+            
+
+class Zipper:
+    def __init__(self, zip_dir):
+        self.process_arg = ["7z", "a", zip_dir]
+
+    def zip_file(self, zip_location):
+        subprocess.call(self.process_arg)
+    
+    def append_argument(self, input_arg):
+        self.process_arg.append(input_arg)
+
+    def get_argument(self):
+        return self.process_arg
+
+
+if __name__ == "__main__":
+    main() 
